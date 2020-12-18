a9bf78a8da5e48cb38091c42780c3fa3d90e6321
hudalizaman <hudalizaman@mail.ugm.ac.id>
2020-12-18 13:31:41 +0700

    Update train.py

--- train.py
+++ train.py
@@ -378,12 +378,12 @@ def train():
 
 # Hyperparameters (results68: 59.9 mAP@0.5 yolov3-spp-416) https://github.com/ultralytics/yolov3/issues/310
 
-hyp = {'giou': 1.0,  # giou loss gain
+hyp_x = {'giou': 1.0,  # giou loss gain
        'cls': 37.4,  # cls loss gain
        'cls_pw': 1.0,  # cls BCELoss positive_weight
        'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
        'obj_pw': 1.0,  # obj BCELoss positive_weight
-       'iou_t': 0.225,  # iou training threshold
+    #    'iou_t': 0.225,  # iou training threshold
        'lr0': 0.001,  # initial learning rate (SGD=5E-3, Adam=5E-4)
        'lrf': -4.,  # final LambdaLR learning rate = lr0 * (10 ** lrf)
        'momentum': 0.937,  # SGD momentum
@@ -457,9 +457,15 @@ if __name__ == '__main__':
         except:
             pass
 
-        print(hyp)
+        print(hyp_x)
+        iou_hyp = [0.1, 0.15, 0.2, 0.25]
 
-        clf = GridSearchCV(logreg, hyperparameters, cv=10)
+        for i in iou_hyp:
+            hyp = hyp_x
+            hyp["iou_t"] = i
+            print(hyp)
+            train()
+        
 
 
 
