8d6447ba9742dccbc79768a56284c1ed21f1408e
hudalizaman <hudalizaman@mail.ugm.ac.id>
2020-12-22 13:48:26 +0700

    Update train.py

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ results_file = 'results.txt'
 
 def train(x):
 
-    # hyp["iou_t"] = x
+    hyp["iou_t"] = x
     cfg = opt.cfg
     data = opt.data
     img_size, img_size_test = opt.img_size if len(opt.img_size) == 2 else opt.img_size * 2  # train, test sizes
@@ -178,7 +178,7 @@ def train(x):
                                              pin_memory=True,
                                              collate_fn=dataset.collate_fn)
 
-    hyp["iou_t"] = x
+    
 
     # Model parameters
     model.nc = nc  # attach number of classes to model
@@ -382,54 +382,6 @@ def train(x):
 
     return best_fitness
 
-def posterior(optimizer, x_obs, y_obs, grid):
-    optimizer._gp.fit(x_obs, y_obs)
-
-    mu, sigma = optimizer._gp.predict(grid, return_std=True)
-    return mu, sigma
-
-def plot_gp(optimizer, x, y):
-    fig = plt.figure(figsize=(16, 10))
-    steps = len(optimizer.space)
-    fig.suptitle(
-        'Gaussian Process and Utility Function After {} Steps'.format(steps),
-        fontdict={'size':30}
-    )
-    
-    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
-    axis = plt.subplot(gs[0])
-    acq = plt.subplot(gs[1])
-    
-    x_obs = np.array([[res["params"]["x"]] for res in optimizer.res])
-    y_obs = np.array([res["target"] for res in optimizer.res])
-    
-    mu, sigma = posterior(optimizer, x_obs, y_obs, x)
-    axis.plot(x, y, linewidth=3, label='Target')
-    axis.plot(x_obs.flatten(), y_obs, 'D', markersize=8, label=u'Observations', color='r')
-    axis.plot(x, mu, '--', color='k', label='Prediction')
-
-    axis.fill(np.concatenate([x, x[::-1]]), 
-              np.concatenate([mu - 1.9600 * sigma, (mu + 1.9600 * sigma)[::-1]]),
-        alpha=.6, fc='c', ec='None', label='95% confidence interval')
-    
-    axis.set_xlim((-2, 10))
-    axis.set_ylim((None, None))
-    axis.set_ylabel('f(x)', fontdict={'size':20})
-    axis.set_xlabel('x', fontdict={'size':20})
-    
-    utility_function = UtilityFunction(kind="ucb", kappa=5, xi=0)
-    utility = utility_function.utility(x, optimizer._gp, 0)
-    acq.plot(x, utility, label='Utility Function', color='purple')
-    acq.plot(x[np.argmax(utility)], np.max(utility), '*', markersize=15, 
-             label=u'Next Best Guess', markerfacecolor='gold', markeredgecolor='k', markeredgewidth=1)
-    acq.set_xlim((-2, 10))
-    acq.set_ylim((0, np.max(utility) + 0.5))
-    acq.set_ylabel('Utility', fontdict={'size':20})
-    acq.set_xlabel('x', fontdict={'size':20})
-    
-    axis.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
-    acq.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
-
 
 
 # Hyperparameters (results68: 59.9 mAP@0.5 yolov3-spp-416) https://github.com/ultralytics/yolov3/issues/310
@@ -578,43 +530,24 @@ if __name__ == '__main__':
         # Bayesian OPT
 
         from bayes_opt import BayesianOptimization
-        from bayes_opt import UtilityFunction
-        import numpy as np
 
-        import matplotlib.pyplot as plt
-        from matplotlib import gridspec
-        # %matplotlib inline
-            
-        hyp = hyp_x    
-        # x = np.linspace(0, 0.5, 10).reshape(-1, 1)
-        # y = train(x)
-        
-        optimizer = BayesianOptimization(train, {'x': (0, 0.5)}, random_state=27)
+        # Parameter Space
+        pbounds = {'x':(0 , 0.5)}
 
-        plot_gp(optimizer, x, y)
+        optimizer = BayesianOptimization(
+            f=train,
+            pbounds = pbounds,
+            verbose = 2,
+            random_state=1
+        )
 
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
+        optimizer.maximize(
+            init_points = 2,
+            n_iter = 5
+        )
 
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
 
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
 
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
-
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
-
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
-
-        optimizer.maximize(init_points=0, n_iter=1, kappa=5)
-        plot_gp(optimizer, x, y)
-
-        
 
 
 
