c17a5a222f104f3a1135256537c227eabc05b687
hudalizaman <hudalizaman@mail.ugm.ac.id>
2021-01-11 18:38:19 +0700

    Update train.py

--- train.py
+++ train.py
@@ -529,7 +529,7 @@ if __name__ == '__main__':
             while iou_lama == iou_baru:
                 iou_baru = 0.5*acak
             else:
-                break
+                pass
 
             map_now = iou_baru ** 3 -4 * iou_baru **2 + 5 * iou_baru +3
             
