22e100071d40c21c6f6f6d5e1bcb7c0260e3ab77
hudalizaman <hudalizaman@mail.ugm.ac.id>
2021-02-01 16:57:13 +0700

    Update train.py

--- train.py
+++ train.py
@@ -480,7 +480,7 @@ if __name__ == '__main__':
         for i in iter:
             # iounya = (i/13) * 0.001
             # iou_hyp.append(iounya)
-            momentumnya = (i/10) +0.9
+            momentumnya = (i/10) + 0.9
             momentum_hyp.append(momentumnya )
 
         # iou_hyp = [0.1,  0.2, 0.3,  0.4,  0.50]
@@ -489,6 +489,7 @@ if __name__ == '__main__':
         best_momentum = 0
         hasil = []
         no = 1
+        print(momentum_hyp)
         for i in momentum_hyp:
             
             hyp = hyp_x
